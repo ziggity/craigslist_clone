740598208f021eebdd57246cb5264861731ffeb2
zach beecher <zachbeecher@gmail.com>
2017-10-30 22:49:08 -0700

    tasks controller with some validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,40 @@
 class TasksController < ApplicationController
-    def index
 
+    before_action :find_task, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @tasks = Task.all.order("created_at DESC")
     end
     
+    def show
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(tasks_params)
+        if @task.save
+            redirect_to @task
+        else
+            render "New"
+        end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private 
+    def tasks_params
+        params_require(:task).permit(:title, :description, :company, :url)
+    end
+
+    def find_task
+        @task = Task.find(params[:id])
+    end
+
 end
